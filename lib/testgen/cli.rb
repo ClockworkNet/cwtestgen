acd6819c0f0262a0171646d794a3cf332f169ebd
Chris Smalley <chrissmalley@me.com>
2013-04-17 10:28:53 -0500

    Stripped out a whole bunch of stuff so now it's just CW specific

--- lib/testgen/cli.rb
+++ lib/testgen/cli.rb
@@ -3,20 +3,9 @@ require 'testgen/generators/project'
 
 module TestGen
   class CLI < Thor
-    
     desc "project <project_name>", "Create a new test project"
-    method_option :pageobject_driver, :type => :string, :required => false, :desc => "Use the PageObject gem to drive browsers.  Valid values are 'watir' and 'selenium'"
-    method_option :with_lib, :type => :boolean, :desc => "Place shared objects under lib directory"
-    method_option :with_gametel, :type => :boolean, :desc => "Add support for gametel gem"
-    method_option :with_mohawk, :type => :boolean, :desc => 'Adds support for mohawk gem'
-    method_option :cw, :type => :boolean, :desc => "Sets up CW cuke environment"
     def project(name)
-      driver = options[:pageobject_driver].nil? ? 'none' : options[:pageobject_driver]
-      with_lib = options[:with_lib] ? 'true' : 'false'
-      with_gametel = options[:with_gametel] ? 'true' : 'false'
-      with_mohawk = options[:with_mohawk] ? 'true' : 'false'
-      cw = options[:cw] ? 'true' : 'false'
-      TestGen::Generators::Project.start([name, driver, with_lib, with_gametel, with_mohawk, cw])
+      TestGen::Generators::Project.start([name])
     end
   end
 end
--- lib/testgen/generators/project.rb
+++ lib/testgen/generators/project.rb
@@ -6,11 +6,6 @@ module TestGen
       include Thor::Actions
     
       argument :name, :type => :string, :desc => 'The name of the project'
-      argument :pageobject_driver, :type => :string, :desc => 'Driver to use with PageObject'
-      argument :with_lib, :type => :string, :desc => 'Place all shared objects in the lib directory'
-      argument :with_gametel, :type => :string, :desc => 'Add support for the gametel gem'
-      argument :with_mohawk, :type => :string, :desc => 'Add support for the mohawk gem'
-      argument :cw, :type => :string, :desc => "Sets up CW cuke environment"
       desc "Generates a project structure for testing with Cucumber"
       
       def self.source_root
@@ -24,14 +19,6 @@ module TestGen
       def copy_cucumber_yml
         template "cucumber.yml.tt", "#{name}/cucumber.yml"
       end
-    
-      def copy_gemfile
-        template "Gemfile.tt", "#{name}/Gemfile" if cw != 'true'
-      end
-      
-      def copy_rakefile
-        copy_file "Rakefile", "#{name}/Rakefile" if cw != 'true'
-      end
       
       def create_cucumber_directories
         empty_directory("#{name}/features")
@@ -40,40 +27,19 @@ module TestGen
       end
 
       def create_log_directory
-        empty_directory("#{name}/logs") if cw == 'true'
+        empty_directory("#{name}/logs")
       end
       
       def copy_env
         template "env.rb.tt", "#{name}/features/support/env.rb"
       end
       
-      def copy_hooks
-        template "hooks.rb.tt", "#{name}/features/support/hooks.rb" if cw != 'true'
-      end
-      
-      def create_lib_directory
-        empty_directory("#{name}/lib") if gen_lib
-      end
-      
       def create_pages_directory
-        if gen_lib
-          empty_directory("#{name}/lib/pages") unless no_driver_selected
-          empty_directory("#{name}/lib/screens") if with_gametel == 'true' or with_mohawk == 'true'
-        else
-          empty_directory("#{name}/features/support/pages") unless no_driver_selected
-          empty_directory("#{name}/features/support/screens") if with_gametel == 'true' or with_mohawk == 'true'
-        end
+        empty_directory("#{name}/features/support/pages")
       end
       
       private
-      
-      def no_driver_selected
-        pageobject_driver.downcase == 'none'
-      end
-      
-      def gen_lib
-        with_lib == 'true'
-      end
+
     end
   end
 end
